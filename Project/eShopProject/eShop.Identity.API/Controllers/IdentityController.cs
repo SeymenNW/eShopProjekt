abb10828bef22892310f0b39220f209601086cbc
seymen <seymennw@gmail.com>
2025-09-30 12:17:21 +0200

    identity api

---
+++ Project/eShopProject/eShop.Identity.API/Controllers/IdentityController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eShop.Identity.API.Controllers
+{
+
+    [ApiController]
+    [Route("/api/identity")]
+    public class IdentityController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Adil skal rejse hjem");
+        }
+    }
+}
