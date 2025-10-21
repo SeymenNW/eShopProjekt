4ea6a56ece38822aad5de9d5d0b930112f5f480d
seymen <seymennw@gmail.com>
2025-10-21 11:39:40 +0200

    fixez

--- Project/eShopProject/eShop.Catalog.API/Controllers/CatalogBrandController.cs
+++ Project/eShopProject/eShop.Catalog.API/Controllers/CatalogBrandController.cs
@@ -4,13 +4,8 @@ namespace eShop.Catalog.API.Controllers
 {
 
     [ApiController]
-<<<<<<< HEAD:Project/eShopProject/eShop.Catalog.API/Controllers/CatalogBrandController.cs
     [Route("api/catalog-brands")]
     public class CatalogBrandController : Controller
-=======
-    [Route("/api/[controller]")]
-    public class CatalogController : Controller
->>>>>>> e6816056a63f71b46247bc252133f9fbbd72e1f4:Project/eShopProject/eShop.Catalog.API/Controllers/CatalogController.cs
     {
         [HttpGet]
         public async Task<IActionResult> GetCatalogBrands()
